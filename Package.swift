c267ea6062eaa25a751449ade1fdeca3d559b3eb
johnny1494 <johnny141994@icloud.com>
2026-02-10 00:08:21 -0800

    Initialize dependabot-combined-prs-1738573423386

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "BranchSDK",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "BranchSDK",
+            targets: ["BranchSDK"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "BranchSDK",
+            path: "Sources",
+            sources: [
+                "BranchSDK/"
+            ],
+            resources: [
+                .copy("Resources/PrivacyInfo.xcprivacy"),
+            ],
+            publicHeadersPath: "BranchSDK/Public/",
+            cSettings: [
+                .headerSearchPath("BranchSDK/Private"),
+            ],
+            linkerSettings: [
+                .linkedFramework("CoreServices"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("WebKit", .when(platforms: [.iOS])),
+                .linkedFramework("CoreSpotlight", .when(platforms: [.iOS])),
+                .linkedFramework("AdServices", .when(platforms: [.iOS]))
+            ]
+        ),
+    ]
+)
